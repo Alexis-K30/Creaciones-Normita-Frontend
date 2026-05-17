24cfba14f7a327a63be32ae99cfa7ea7c5fcb18b
marceloguerrero-debug <marcelorodriguezz2034@gmail.com>
2026-05-16 18:34:55 -0600

    Add files via upload

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -26,7 +26,7 @@ const Register = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:8080/api/auth/register", {
+      const response = await fetch("http://localhost:8082/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
